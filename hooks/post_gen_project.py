ac10649b323933c44fb27b69a3d2d72c175bd391
Brandon Corfman <bcorfman@fastmail.fm>
2024-05-17 23:47:31 -0400

    Finished first version

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 """Script that runs after the project generation phase."""
-import sys
-from subprocess import call
 from pathlib import Path
+from subprocess import call, run, CompletedProcess, CalledProcessError
 
 PROJECT_DIRECTORY = Path.cwd()
+MODULE_NAME = "{{ cookiecutter.repo_name }}"
 
 brew_working = False
 gh_cli_and_rye_installed = False
@@ -17,21 +17,44 @@ git_rename_branch = False
 git_create_repo = False
 
 
-def subproc(cmd):
+def subproc(args):
     success = False
+    proc = CompletedProcess('', 0)
     try:
-        retcode = call(cmd, shell=True)
+        retcode = call(args, shell=True)
         if retcode < 0:
-            print("Child was terminated by signal", -retcode, file=sys.stderr)
+            print("Child was terminated by signal", -retcode, file=proc.stderr)
         else:
             if retcode != 0:
-                print("Child returned", retcode, file=sys.stderr)
+                print("Child returned", retcode, file=proc.stderr)
             else:
                 success = True
     except OSError as e:
-        print("Execution failed:", e, file=sys.stderr)
+        print("Execution failed:", e, file=proc.stderr)
     
     return success
+
+
+def subproc_with_output(cmd, **args):
+    success = False
+    proc = CompletedProcess('', 0)
+    try:
+        proc = run(cmd, shell=True, text=True, capture_output=True, check=True)
+        if proc.returncode < 0:
+            print("Child was terminated by signal", -proc.returncode, file=proc.stderr)
+        else:
+            if proc.returncode != 0:
+                print("Child returned", proc.returncode, file=proc.stderr)
+            else:
+                success = True
+    except OSError as e:
+        if not args.get('suppress_failure'):
+            print("Execution failed:", e, file=proc.stderr)
+    except CalledProcessError as e:
+        if not args.get('suppress_failure'):
+            print("Execution failed:", e, file=proc.stderr)
+    return success, proc.stdout
+
 
 def main():
     # The basics: you'll need Homebrew on your system. 
@@ -39,55 +62,87 @@ def main():
     # so it's a prerequisite instead. If you'd rather use some other package manager
     # that works on both Mac and Linux, this is the place to make sure it's installed 
     # and working. 
-    brew_working = subproc("brew update")
-    if not brew_working:
-        print("Could not perform brew update.")
+    brew_working, stdout = subproc_with_output("brew --version")
+    if not brew_working or "Homebrew " not in stdout:
+        print("Could not find Homebrew on your system.")
         return
-    
-    # install GitHub CLI and Rye.
-    gh_cli_and_rye_installed = subproc("brew install gh rye")
-    if not gh_cli_and_rye_installed:
-        print("Could not perform brew install for GitHub CLI and Rye.")
+
+    # if GitHub CLI and Rye aren't already installed, install them!
+    _, stdout = subproc_with_output("brew list")
+    output = stdout.split()
+    if 'gh' not in output: 
+        gh_cli_installed = subproc("brew install gh")
+        if not gh_cli_installed:
+            print("Could not perform brew install for GitHub CLI.")
+            return 
+    if 'rye' not in output: 
+        rye_installed = subproc("brew install rye")
+        if not rye_installed:
+            print("Could not perform brew install for Rye.")
+            return 
+   
+    username = None
+    pat = None
+    get_pat, stdout = subproc_with_output("echo url=https://github.com | git credential fill")
+    if not get_pat:
+        print("Couldn't obtain GitHub PAT from Git Credential Manager. Have you used it before?")
+        return
+    else:
+        for line in stdout.splitlines():
+            if "username=" in line:
+                username = line.split("=")[-1].strip()
+            if "password=" in line:
+                pat = line.split("=")[-1].strip()
+                break
+    if not pat or not username:
+        print("Could not obtain GitHub username and PAT from Git Credential Manager. Have you used it before?")
+        return
+
+    # login to GitHub and add/commit/push the newly created repo
+    cmd = f"echo {pat} | gh auth login --with-token"
+    auth_to_github = subproc(cmd)
+    if not auth_to_github:
+        print("Could not authenticate to GitHub. Check PAT content.")
         return 
-    
-    # rye init does git init for the repo, along with some other config. 
-    # (see Rye docs for complete details).
-    git_initialized = subproc("git init")
+
+    repo_check_fail, stdout = subproc_with_output(f'git ls-remote -h "https://github.com/{username}/{MODULE_NAME}.git" &> /dev/null',
+                                                  suppress_failure=True)
+    if repo_check_fail:
+        print(f"Found an existing repo called {MODULE_NAME} in your GitHub account. Please delete before continuing.")
+        return
+
+    git_initialized, _ = subproc_with_output("git init")
     if not git_initialized:
         print("Could not perform git init in project directory.")
         return 
 
-    install_complete = subproc("make devinstall")
+    install_complete, _ = subproc_with_output("make devinstall")
     if not install_complete:
         print("Could not make development environmnent for project.")
         return 
 
-    # login to GitHub and add/commit/push the newly created repo
-    auth_to_github = subproc("gh auth login --with-token < pat.txt")
-    if not auth_to_github:
-        print("Could not authenticate to GitHub. Check content and location of PAT.")
-        return 
-
-    git_add = subproc("git add .")
+    git_add, _ = subproc_with_output("git add .")
     if not git_add:
         print("Could not perform git add on project files.")
         return 
             
-    git_commit = subproc('git commit -m "Initial commit"')
+    git_commit, _ = subproc_with_output('git commit -m "Initial commit"')
     if not git_commit:
         print("Could not perform initial git commit.")
         return 
             
-    git_rename_branch = subproc("git branch -M main")
+    git_rename_branch, _ = subproc_with_output("git branch -M main")
     if not git_rename_branch:
         print("Could not rename master branch to main.")
         return 
             
-    git_create_repo = subproc("gh repo create {{ cookiecutter.repo_name }} --public --push --source=.")
+    git_create_repo, _ = subproc_with_output("gh repo create {{ cookiecutter.repo_name }} --public --push --source=.")
     if not git_create_repo:
         print("Could not create repo on GitHub. Check project name.")
         return 
 
-
+    print("Done!")
+    
 if __name__ == "__main__":
     main()
+
